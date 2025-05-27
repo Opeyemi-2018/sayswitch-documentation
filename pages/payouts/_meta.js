de153d222aa705430fcfe6fde6a4b77e799e2309
Opeyemi odunayo <127036369+Opeyemi-2018@users.noreply.github.com>
2025-05-27 10:46:58 +0100

    change

--- pages/payouts/_meta.js
+++ pages/payouts/_meta.js
@@ -1,7 +1,6 @@
 export default {
   getBankList: "Get Bank List",
   accountNameVerification: "Account Name Verification",
-  initiatePayout: "Initiate Payout",
   getPayouts: "Get Payouts",
   getPayoutDetails: "⁠Get Payout Details",
 };
