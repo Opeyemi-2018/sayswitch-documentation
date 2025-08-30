1d10c8e3a537c107eb4b2800bb4274282d80caef
Opeyemi odunayo <127036369+Opeyemi-2018@users.noreply.github.com>
2025-08-30 14:27:35 +0100

    added payout and bulk payout page

--- pages/payouts/_meta.js
+++ pages/payouts/_meta.js
@@ -1,6 +1,8 @@
 export default {
   getBankList: "Get Bank List",
   accountNameVerification: "Account Name Verification",
+  initiatePayout: "Initiate Payout",
+  initiateBulkPayout: "Initiate Bulk Payout",
   getPayouts: "Get Payouts",
   getPayoutDetails: "⁠Get Payout Details",
 };
