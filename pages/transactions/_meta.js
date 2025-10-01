d4f6524e06879a57d3103f75047f739833a36b26
Sam <owolabisamuel15@gmail.com>
2025-10-01 14:33:19 +0100

    Get Transaction By Reference, Webhook Signature Veirfication, Test Encryption

--- pages/transactions/_meta.js
+++ pages/transactions/_meta.js
@@ -1,6 +1,7 @@
 export default {
   overview: "Overview",
   getTransactionDetails: "⁠Get Transaction Details",
+  gettransactionbyreference: "Get Transaction by Reference",
   transactionTimeline: "Transaction Timeline",
   transactionTotal: "Transaction Total",
 };
