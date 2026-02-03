3dbf3129edc400edbdf2621d0abc608e5c04cddd
opeyemi-2018 <opeyemiodunayo2018@gmail.com>
2026-02-03 01:15:41 +0100

    added bulk customer creation endpoint

--- pages/customers/_meta.js
+++ pages/customers/_meta.js
@@ -1,6 +1,7 @@
 export default {
   overview: "Overview",
   createCustomer: "Create Customer",
+  createBulkCustomer: "Create Bulk Customer",
   getCustomers: "Get Customers",
   getCustomerDetails: "Get Customer Details",
 };
--- pages/virtualAccount/_meta.js
+++ pages/virtualAccount/_meta.js
@@ -1,6 +1,7 @@
 export default {
   overview: "Overview",
   createVirtualAccount: "Create Virtual Account",
+    createBulkVirtualAccount: "Create Bulk Virtual Account",
   getVirtualAccount: "Get Virtual Accounts",
   getVirtualAccountDetails: "Get Virtual Account Details",
 };
